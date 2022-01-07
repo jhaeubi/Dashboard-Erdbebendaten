ba10946a33a18f98b8729b0911affe1d6bff9e82
jhaeubi <79747491+jhaeubi@users.noreply.github.com>
2022-01-07 11:07:22 +0100

    Anpassung Titel,Achsenbeschrieb

--- Dashboard.py
+++ Dashboard.py
@@ -27,24 +27,24 @@ app.layout = html.Div([
     dcc.Dropdown(id='Dropdown',
                  options=[{'label': i, 'value': i} for i in df_small['Date'].unique()],
                  multi=True,
-                 style={"width": "70%"}),
+                 style={"width": "70%", 'textAlign': 'left'}), #eventuell nur Monat
 
     dcc.Slider(id='my-slider',
-                    min=0, max=8, step=0.1, value=0,
-                    tooltip={'always_visible': True},
-                    marks={0.5 * i: '{}'.format(0.5 * i) for i in range(16)}),
+               min=0, max=8, step=0.1, value=0,
+               tooltip={'always_visible': True},
+               marks={0.5 * i: '{}'.format(0.5 * i) for i in range(16)}),
 
-    #dcc.RangeSlider(id='my-ranged-slider',
-     #          min=0, max=8, step=0.1, value=0,
-      #         tooltip={'always_visible': True},
-       #        marks={0.5 * i: '{}'.format(0.5 * i) for i in range(16)}),
+    # dcc.RangeSlider(id='my-ranged-slider',
+    #          min=0, max=8, step=0.1, value=0,
+    #         tooltip={'always_visible': True},
+    #        marks={0.5 * i: '{}'.format(0.5 * i) for i in range(16)}),
 
     html.Div(children=[
         dcc.Graph(id='Magnitude', figure={}),
         dcc.Graph(id='Depth', figure={})],
         style={'display': 'inline-block', 'vertical-align': 'topleft',
-                'margin-left': '2.5vw', 'margin-top': '2.5vw',
-                'width': '25vw', 'height': '45vh'}),
+               'margin-left': '2.5vw', 'margin-top': '2.5vw',
+               'width': '25vw', 'height': '45vh'}),
 
     html.Div(children=[
         dcc.Graph(id='WorldMap', figure={})],
@@ -63,14 +63,14 @@ app.layout = html.Div([
 
 
 @app.callback(
-    [#Output(component_id='output-container-range-slider', component_property='figure'),
-    Output(component_id='Magnitude', component_property='figure'),
-     Output(component_id='Depth', component_property='figure'),
-     Output(component_id='WorldMap', component_property='figure'),
-     Output(component_id='Barplot', component_property='figure'),
-     Output(component_id='Lineplot', component_property='figure')],
+    [  # Output(component_id='output-container-range-slider', component_property='figure'),
+        Output(component_id='Magnitude', component_property='figure'),
+        Output(component_id='Depth', component_property='figure'),
+        Output(component_id='WorldMap', component_property='figure'),
+        Output(component_id='Barplot', component_property='figure'),
+        Output(component_id='Lineplot', component_property='figure')],
     [Input(component_id='Dropdown', component_property='value'),
-     Input(component_id='my-slider', component_property='value'),]
+     Input(component_id='my-slider', component_property='value'), ]
 )
 def update_graph(option_slctd, option_slctd2):
     dff = df_small.copy()
@@ -79,31 +79,42 @@ def update_graph(option_slctd, option_slctd2):
         dff = dff[dff["Date"].isin(option_slctd)]
     if option_slctd2 > 0:
         dff = dff[dff['mag'] == option_slctd2]
-        #dff = dff[dff['mag'].between(option_slctd2[0], option_slctd2[1], inclusive=True)]
+        # dff = dff[dff['mag'].between(option_slctd2[0], option_slctd2[1], inclusive=True)]
 
     fig = px.box(dff, x='Month', y='mag')
+    fig.update_layout(title={'text':'Boxplot Magnitude und Monat'},
+                      yaxis={'title':'Magnitude'},
+                      xaxis={'title':'Month'})
 
     fig2 = px.density_heatmap(dff, x="mag", y="depth", marginal_x="rug", marginal_y="histogram")
+    fig2.update_layout(title={'text': 'Vergleich Magnitude, Depth mit Anzahl Erdbeben'},
+                       yaxis={'title': 'Depth'},
+                       xaxis={'title': 'Magnitude'})
+
+
 
     fig3 = px.density_mapbox(dff, lat='latitude', lon='longitude', z='mag',
-                            hover_name='place', hover_data=['Date'],
-                            radius=10, center=dict(lat=0, lon=0), zoom=0, mapbox_style="stamen-terrain")
+                             hover_name='place', hover_data=['Date'],
+                             radius=10, center=dict(lat=0, lon=0), zoom=0, mapbox_style="stamen-terrain")
+    fig3.update_layout(title={'text': 'Data around the World'})
 
+    # fig4 = px.bar_polar(dff, r="mag", theta="latitude", color="depth", template="plotly_dark",
+    # color_discrete_sequence=px.colors.sequential.Plasma_r)
 
-    #fig4 = px.bar_polar(dff, r="mag", theta="latitude", color="depth", template="plotly_dark",
-                       #color_discrete_sequence=px.colors.sequential.Plasma_r)
-
-    #fig4 = px.line_polar(dff, r="mag", theta="longitude", color="depth", line_close=True,
-                        #color_discrete_sequence=px.colors.sequential.Plasma_r)
+    # fig4 = px.line_polar(dff, r="mag", theta="longitude", color="depth", line_close=True,
+    # color_discrete_sequence=px.colors.sequential.Plasma_r)
 
     fig4 = px.bar(dff, x='Month', y='mag',
-                    color='mag',
-                    labels={'mag':'Magnitude'}, height=400)
+                  color='mag',
+                  labels={'mag': 'Magnitude'}, height=400)
+    fig4.update_layout(title={'text': 'Anzahl der Erdbeben pro Magnitude'},
+                       yaxis={'title': 'Anzahl'},
+                       xaxis={'title': 'Month'})
 
     fig5 = px.scatter_polar(dff, r="Month", theta="Region",
                             color="mag",
                             color_discrete_sequence=px.colors.sequential.Plasma_r, template="plotly_dark")
-
+    fig5.update_layout(title={'text': 'Polarchart zum Vergleich der Regionen'})
 
 
     """fig5 = px.parallel_coordinates(dff,
