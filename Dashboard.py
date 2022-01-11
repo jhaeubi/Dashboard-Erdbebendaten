e894fb3d338d6cf4a647cb5a786245c5d0aa9c96
jhaeubi <79747491+jhaeubi@users.noreply.github.com>
2022-01-11 15:17:04 +0100

    Anpassungen Layout, Schreibfehler

--- Dashboard.py
+++ Dashboard.py
@@ -21,7 +21,7 @@ df_small['Time'] = df_small['Time'].map(lambda x: str(x)[:-1])
 
 markdown_text = """
 ## Earthquakes around the World
-### Facts and Figures about Earthquakes around the World January 1st 2021 to July 1st 2021.
+### Facts and figures about earthquakes in the first half of the year 2021
 
 **Time Period:** 1.1.2021 to 1.07.2021  
 **Area:**            Worldwide  
@@ -40,7 +40,7 @@ app.layout = html.Div([
     # html.H4("Facts and Figures about Earthquakes around the World January - July 2020", style={'text-align': 'left'}),
     html.P(),
 
-    html.Label('Choose Month'),
+    html.Label('Choose Month', style={'font': 'Arial'}),
 
     dcc.Dropdown(id='Dropdown',
                  options=[
@@ -55,7 +55,7 @@ app.layout = html.Div([
                  multi=True,
                  style={"width": "70%", 'textAlign': 'left'}),
 
-    html.Label('Choose Magnitude'),
+    html.Label('Choose Magnitude', style={'font': 'Arial'}),
 
     # dcc.Slider(id='my-slider',
     #           min=0, max=8, step=0.1, value=0,
@@ -81,21 +81,21 @@ app.layout = html.Div([
         dcc.Graph(id='Depth', figure={})],
         style={'display': 'inline-block', 'vertical-align': 'topleft',
                'margin-left': '2.5vw', 'margin-top': '2.5vw',
-               'width': '25vw', 'height': '45vh'}),
+               'width': '25vw', 'height': '45vh', 'font': 'Arial'}),
 
     html.Div(children=[
         dcc.Graph(id='WorldMap', figure={}),
         dcc.Markdown(id='my-markdown', children=markdown_text)],
         style={'display': 'inline-block', 'vertical-align': 'middle',
                'margin-left': '2.5vw', 'margin-top': '2.5vw',
-               'width': '40vw', 'height': '95vh'}),
+               'width': '40vw', 'height': '95vh', 'font': 'Arial'}),
 
     html.Div(children=[
         dcc.Graph(id='Barplot', figure={}),
         dcc.Graph(id='Lineplot', figure={})],
         style={'display': 'inline-block', 'vertical-align': 'topright',
                'margin-left': '2.5vw', 'margin-top': '2.5vw',
-               'width': '25vw', 'height': '45vh'})
+               'width': '25vw', 'height': '45vh', 'font': 'Arial'})
 
 ])
 
@@ -115,35 +115,47 @@ def update_graph(option_slctd, option_slctd2):
     if bool(option_slctd):
         dff = dff[dff["Month"].isin(option_slctd)]
 
-    dff = dff[dff['magnitude'].between(option_slctd2[0], option_slctd2[1], inclusive=True)]
+    dff = dff[dff['Magnitude'].between(option_slctd2[0], option_slctd2[1], inclusive=True)]
 
-    fig = px.box(dff, x='Month', y='magnitude')
+    fig = px.box(dff, x='Month', y='Magnitude')
     fig.update_layout(title={'text': 'Mean values of magnitude per month'},
                       yaxis={'title': 'Magnitude'},
-                      xaxis={'title': 'Month'})
+                      xaxis={'title': 'Month'},
+                      font=dict(family='Arial'))
 
-    fig2 = px.density_heatmap(dff, x="magnitude", y="depth", marginal_x="rug", marginal_y="histogram", template='plotly_dark')
+    fig2 = px.density_heatmap(dff, x="Magnitude", range_x=(0, 8),
+                              y="Depth",
+                              marginal_x="rug", marginal_y="histogram",
+                              template='plotly_dark')
+
     fig2.update_layout(title={'text': 'Comparison magnitude and depth, count of quakes'},
                        yaxis={'title': 'Depth'},
-                       xaxis={'title': 'Magnitude'})
+                       xaxis={'title': 'Magnitude'},
+                       font=dict(family='Arial'),
+                       coloraxis_colorbar=dict(
+                           title="Count")
+                       )
 
-    fig3 = px.density_mapbox(dff, lat='latitude', lon='longitude', z='magnitude',
+    fig3 = px.density_mapbox(dff, lat='Latitude', lon='Longitude', z='Magnitude',
                              hover_name='place', hover_data=['Date'],
-                             radius=10, center=dict(lat=0, lon=0), zoom=0, mapbox_style="stamen-terrain", template='plotly_dark')
-    fig3.update_layout(title={'text': 'World map with all the data(magnitude by color)'})
+                             radius=10, center=dict(lat=0, lon=0), zoom=0, mapbox_style="stamen-terrain",
+                             template='plotly_dark')
+    fig3.update_layout(title={'text': 'World map with place, magnitude, latitude, longitude and date'},
+                       font=dict(family='Arial'))
 
-    fig4 = px.bar(dff, x='Month', y='magnitude',
-                  color='magnitude',
-                  labels={'mag': 'Magnitude'}, height=450)
+    fig4 = px.bar(dff, x='Month', y='Magnitude',
+                  color='Magnitude',
+                  labels={'Magnitude': 'Magnitude'}, height=450)
     fig4.update_layout(title={'text': 'Number of earthquakes per magnitude'},
                        yaxis={'title': 'Count'},
-                       xaxis={'title': 'Month'})
+                       xaxis={'title': 'Month'},
+                       font=dict(family='Arial'))
 
     fig5 = px.scatter_polar(dff, r="Month", theta="Region",
-                            color="magnitude",
+                            color="Magnitude",
                             color_discrete_sequence=px.colors.sequential.Plasma_r, template="plotly_dark")
-    fig5.update_layout(title={'text': 'Comparison of the regions per magnitude'})
-
+    fig5.update_layout(title={'text': 'Comparison of the regions per magnitude'},
+                       font=dict(family='Arial'))
 
     return fig, fig2, fig3, fig4, fig5
 
